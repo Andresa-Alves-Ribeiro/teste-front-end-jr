b90be10311616af5c3295757f0438e4ba9aeeb17
Andresa Alves <andresa_15ga@hotmail.com>
2023-06-13 14:32:50 -0300

    Iniciando projeto e importando os svgs

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import { Header } from './components/header/header'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+      <div>
+        <p>teste</p>
+      </div>
+    </>
+  )
+}
+
+export default App
